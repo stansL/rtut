6ce917afe8f14ca2c9ab7cd16ab53b922296e91d
Stanslaus <mstans8@gmail.com>
2020-01-08 23:25:39 +0300

    add functionality to load a new todo to the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 import Header from './components/layout/Header';
 
 class App extends Component {
@@ -24,7 +25,7 @@ class App extends Component {
       }
     ]
   }
-// Toggle complete
+  // Toggle complete
   markComplete = (id) => {
     this.setState({
       todos: this.state.todos.map(todo => {
@@ -44,11 +45,26 @@ class App extends Component {
     });
   };
 
+  addTodo = (title) => {
+    this.setState({
+      todos: [...this.state.todos,{
+        id: this.state.todos.length + 1,
+        title,
+        completed: false
+      }]
+
+    });
+
+  }
   render() {
     return (
       <div className="App">
-        <Header />
-        <Todos todos={this.state.todos} markComplete={this.markComplete} deleteItem = {this.deleteItem}/>
+        <div className="container">
+          <Header />
+          <Todos todos={this.state.todos} markComplete={this.markComplete} deleteItem={this.deleteItem} />
+          <AddTodo addTodo={this.addTodo} />
+        </div>
+
       </div>
     );
   }
---
+++ src/components/AddTodo.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class AddTodo extends Component {
+    state = {
+        title: ""
+    }
+
+    onChange = (e) => this.setState({[e.target.name]: e.target.value })
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.addTodo(this.state.title);
+        this.setState({title: ''});
+
+
+    }
+
+    render() {
+        return (
+            <form style={{ display: 'flex' }} onSubmit = {this.onSubmit}>
+                <input type="text" name="title" placeholder="Add Todo Item" required={true} style={{ flex: '10' }} value={this.state.title}
+                    onChange={this.onChange} />
+                <input type="submit" value="Submit" className="btn" style={{ 'flex': '1' }} />
+            </form>
+        );
+
+    }
+}
+
+export default AddTodo;
