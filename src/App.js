9e0105ecba411bc6111f0662507d97be6d8eec57
Stanslaus <mstans8@gmail.com>
2020-01-08 22:33:28 +0300

    add listing, selection and deletion functionality to the todo lits

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Todos from './components/Todos';
+import Header from './components/layout/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Follow up on Sacco things',
+        completed: false
+      }
+    ]
+  }
+// Toggle complete
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+
+      })
+    });
+  }
+
+  deleteItem = (id) => {
+    this.setState({
+      todos: this.state.todos.filter(todo => todo.id !== id)
+
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} deleteItem = {this.deleteItem}/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
